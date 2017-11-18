46188ddbcadeb9e61a1248d4d0a33cf36efe9747
kingksin <k4eva1123@gmail.com>
2017-11-18 15:58:17 +0900

    std ver 1.0
    djhf

---
+++ src/test02/TestSum.java
@@ -0,0 +1,12 @@
+package test02;
+
+public class TestSum {
+	public static void main(String[] args) {
+		
+		int a =5;
+		int b =3;
+		int c = a +b;
+		
+		System.out.println("a와 b의 합은 ?" +c);
+	}
+}
